34103ae60b32cff77eabef40e239bce8bc99aaa1
wjx0912 <80101277@qq.com>
2021-04-30 10:41:35 +0800

    fix

--- demo/demo-rapidjson/build.bat
+++ demo/demo-rapidjson/build.bat
@@ -1,10 +1,10 @@
 rm build -fr
 
-cmake  -B  build\Win32       -G "Visual Studio 16 2019"  -A Win32    -DCMAKE_USE_OPENSSL=ON
+cmake  -B  build\Win32       -G "Visual Studio 16 2019"  -A Win32
 cmake --build build\Win32    --target ALL_BUILD
 cmake --build build\Win32    --target ALL_BUILD   --config Release
 
-cmake  -B  build\x64         -G "Visual Studio 16 2019"  -A x64      -DCMAKE_USE_OPENSSL=ON
+cmake  -B  build\x64         -G "Visual Studio 16 2019"  -A x64
 cmake --build build\x64      --target ALL_BUILD
 cmake --build build\x64      --target ALL_BUILD   --config Release
 pause
